6d4e6cdbdd5b47991ca91ba156ca7cb355015d81
yuema <1106701542@qq.com>
2019-09-15 00:40:40 +0800

    SpringBoot学习第一步：框架搭建

---
+++ src/main/java/com/ma/yue01/Yue01Application.java
@@ -0,0 +1,13 @@
+package com.ma.yue01;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Yue01Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Yue01Application.class, args);
+	}
+
+}
